d08a00f848ff7badb1aefc29749030eb24b65387
altus34 <altus34@gmail.com>
2017-06-11 20:52:21 -0400

    Clean dependencies to keep the test webdriver only

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # webdriver-issues
 
 Project who isolate sample of possible webdriver issues
+
+1 - Install the Webdriver Edge driver in:
+    C:\Program Files (x86)\Microsoft Web Driver\MicrosoftWebDriver.exe
+
+2 - Install maven
+
+3 - To run the test execute
+    mvn test
+
+N.B:
+ - If you pass this test in debug mode line by line it pass !!!
+ - If you run this test with chromedriver it pass
+ - If you run this test with geckodriver it pass
--- src/test/groovy/com/ovea/testatoo/sample/issue/EdgeIssuesTest.groovy
+++ src/test/groovy/com/ovea/testatoo/sample/issue/EdgeIssuesTest.groovy
@@ -1,26 +1,40 @@
 package com.ovea.testatoo.sample.issue
 
-import com.ovea.testatoo.sample.WebDriverConfig
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.DefaultHandler
+import org.eclipse.jetty.server.handler.HandlerList
+import org.eclipse.jetty.server.handler.ResourceHandler
+import org.junit.AfterClass
 import org.junit.BeforeClass
-import org.junit.ClassRule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
+import org.openqa.selenium.edge.EdgeDriver
 
 /**
  * @author David Avenante (d.avenante@gmail.com)
  */
 @RunWith(JUnit4)
 class EdgeIssuesTest {
-    @ClassRule
-    public static WebDriverConfig config = new WebDriverConfig()
+    static WebDriver webDriver
 
     @BeforeClass
     static void before() {
-        config.webDriver.get('http://localhost:8080/index.html')
+        startJetty()
+
+        System.setProperty('webdriver.edge.driver', 'C:\\Program Files (x86)\\Microsoft Web Driver\\MicrosoftWebDriver.exe')
+        webDriver = new EdgeDriver()
+
+        webDriver.get('http://localhost:8080/index.html')
+    }
+
+    @AfterClass
+    static void after() {
+        webDriver.quit()
+        server.stop()
     }
 
     @Test
@@ -35,5 +49,20 @@ class EdgeIssuesTest {
 
         _checkBox.click()
         assert !_checkBox.selected
+    }
+
+    private static void startJetty() {
+        server = new Server(8080)
+        ResourceHandler resource_handler = new ResourceHandler()
+
+        resource_handler.directoriesListed = true
+        resource_handler.welcomeFiles = ['index.html']
+        resource_handler.resourceBase = 'src/test/webapp'
+
+        HandlerList handlers = new HandlerList()
+        handlers.handlers = [resource_handler, new DefaultHandler()]
+        server.handler = handlers
+
+        server.start()
     }
 }
